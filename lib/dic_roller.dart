51ca2e0f35e9e9660d269e31ad8cced24a06978e
A.sherif <abdelrahman.sherif@4insite.com>
2025-02-23 16:35:30 +0200

    2nd commit

--- lib/dic_roller.dart
+++ lib/dic_roller.dart
@@ -24,6 +24,7 @@ class _DiceRollerState extends State<DiceRoller> {
 
   @override
   Widget build(BuildContext context) {
+    //
     return Column(
       mainAxisSize: MainAxisSize.min,
       children: [
